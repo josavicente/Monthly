ad6db9858de21c57f52175ff1571e1923121f405
Josa Vicente <jvicenpe@gmail.com>
2023-01-26 20:47:08 +0100

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Josafat Vicente Pérez on 26/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
